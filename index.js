6ac6e70f83e1a76d97d31d25b45cfbc79cb8127d
rop-lab <jhnycheru11@gmail.com>
2023-12-31 10:44:38 +0300

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-function addingEventListener() {
+const mainElement = document.getElementById('main'); // Get the main element by its ID
+
+if (mainElement) {
+    mainElement.remove(); // Remove the main element if it exists
 }
+
